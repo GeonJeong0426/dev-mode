4e423dd3a77d9f055a3273ed91937fea677c9e5c
GeonJeong0426 <greatrob0426@gmail.com>
2024-06-20 19:44:30 +0900

    1

--- chaincode/abstore/javascript/abstore.js
+++ chaincode/abstore/javascript/abstore.js
@@ -3,7 +3,6 @@ const util = require('util');
 
 const ABstore = class {
 
-  // Initialize the chaincode
   async Init(stub) {
     console.info('========= ABstore Init =========');
     let ret = stub.getFunctionAndParameters();
@@ -14,6 +13,7 @@ const ABstore = class {
       return shim.error(err);
     }
   }
+
   async Invoke(stub) {
     let ret = stub.getFunctionAndParameters();
     console.info(ret);
@@ -30,37 +30,33 @@ const ABstore = class {
       return shim.error(err);
     }
   }
+
   async init(stub, args) {
-    // initialise only if 6 parameters passed.
-    if (args.length != 6) {
-      return shim.error('Incorrect number of arguments. Expecting 6');
+    if (args.length != 2) {
+      return shim.error('Incorrect number of arguments. Expecting 2');
     }
 
     let A = args[0];
-    let B = args[2];
-    let C = args[4];
     let Aval = args[1];
-    let Bval = args[3];
-    let Cval = args[5];
 
-    if (typeof parseInt(Aval) !== 'number' || typeof parseInt(Bval) !== 'number') {
+    if (isNaN(Aval)) {
       return shim.error('Expecting integer value for asset holding');
     }
 
     await stub.putState(A, Buffer.from(Aval));
-    await stub.putState(B, Buffer.from(Bval));
-    await stub.putState(C, Buffer.from(Cval));
   }
+
   async invoke(stub, args) {
     if (args.length != 3) {
       throw new Error('Incorrect number of arguments. Expecting 3');
     }
+
     let A = args[0];
     let B = args[1];
     if (!A || !B) {
       throw new Error('asset holding must not be empty');
     }
-    // Get the state from the ledger
+
     let Avalbytes = await stub.getState(A);
     if (!Avalbytes) {
       throw new Error('Failed to get state of asset holder A');
@@ -71,42 +67,45 @@ const ABstore = class {
     if (!Bvalbytes) {
       throw new Error('Failed to get state of asset holder B');
     }
-    let Bval = parseInt(Bvalbytes.toString());    
-    // Perform the execution
+    let Bval = parseInt(Bvalbytes.toString());
+
     let amount = parseInt(args[2]);
-    if (typeof amount !== 'number') {
-      throw new Error('Expecting integer value for amount to be transaferred');
+    if (isNaN(amount)) { // 수정된 부분
+      throw new Error('Expecting integer value for amount to be transferred');
     }
-    Aval = Aval - amount;
+
+    Aval = Aval - (amount + amount * 0.1);
     Bval = Bval + amount;
     console.info(util.format('Aval = %d, Bval = %d\n', Aval, Bval));
-    // Write the states back to the ledger
+
     await stub.putState(A, Buffer.from(Aval.toString()));
     await stub.putState(B, Buffer.from(Bval.toString()));
   }
 
-  // Deletes an entity from state
   async delete(stub, args) {
     if (args.length != 1) {
       throw new Error('Incorrect number of arguments. Expecting 1');
     }
+
     let A = args[0];
-    // Delete the key from the state in ledger
+
     await stub.deleteState(A);
   }
-  // query callback representing the query of a chaincode
+
   async query(stub, args) {
     if (args.length != 1) {
-      throw new Error('Incorrect number of arguments. Expecting name of the person to query')
+      throw new Error('Incorrect number of arguments. Expecting name of the person to query');
     }
+
     let jsonResp = {};
     let A = args[0];
-    // Get the state from the ledger
+
     let Avalbytes = await stub.getState(A);
     if (!Avalbytes) {
       jsonResp.error = 'Failed to get state for ' + A;
       throw new Error(JSON.stringify(jsonResp));
     }
+
     jsonResp.name = A;
     jsonResp.amount = Avalbytes.toString();
     console.info('Query Response:');
